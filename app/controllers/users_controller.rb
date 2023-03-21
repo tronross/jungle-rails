0f5098315e92f9745a770803b8d6f9802bce7bc3
tronross <john_ross@analogforever.ca>
2023-03-20 20:32:25 -0400

    Fix redirect_to route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to 'users/new'
+      redirect_to '/users/new'
     end
   end
 
