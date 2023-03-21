9188af218fd330b21f452c02912ce648a78f109a
tronross <john_ross@analogforever.ca>
2023-03-21 16:55:25 -0400

    Lint spacing and indentation

--- config/routes.rb
+++ config/routes.rb
@@ -19,8 +19,6 @@ Rails.application.routes.draw do
   resources :orders, only: [:create, :show]
 
   resources :users, only: [:new, :create]
-  
-  # resources :sessions, only: [:new, :create, :destroy]
 
   namespace :admin do
     root to: 'dashboard#show'
