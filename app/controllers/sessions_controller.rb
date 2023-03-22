0acccbbe56592ec5417ae58c84f7aca857ad9686
tronross <john_ross@analogforever.ca>
2023-03-22 11:18:50 -0400

    Add alert for invalid login to redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
+      flash[:alert] = 'Invalid Login - please re-enter'
       redirect_to '/sessions/new'
     end
   end
