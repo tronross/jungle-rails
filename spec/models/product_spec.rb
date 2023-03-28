c877d98e61311c1332e83135267895fc7216907c
tronross <john_ross@analogforever.ca>
2023-03-28 18:39:06 -0400

    Lint product_spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     it 'saves the correct name' do
-    @category = Category.new
-    @product = Product.new({
-      name: 'Boston',
-      price: 26.99,
-      quantity: 5,
-      category: @category
-    })
-    expect(@product.name).to eq("Boston")
-  end
+      @category = Category.new
+      @product = Product.new({
+        name: 'Boston',
+        price: 26.99,
+        quantity: 5,
+        category: @category
+      })
+      expect(@product.name).to eq("Boston")
+    end
 
     it 'saves the correct price' do
       @category = Category.new
@@ -23,7 +23,7 @@ RSpec.describe Product, type: :model do
       })
       expect(@product.price).to eq(Money.new(2699, :CAD))
     end
-  
+
     it 'saves the correct quantity' do
       @category = Category.new
       @product = Product.new({
@@ -34,7 +34,7 @@ RSpec.describe Product, type: :model do
       })
       expect(@product.quantity).to eq(5)
     end
-  
+
     it 'saves the correct category' do
       @category = Category.new
       @product = Product.new({
