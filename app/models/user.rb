61858c6461e98e20cef6ea16bc2cdce188de1047
tronross <john_ross@analogforever.ca>
2023-03-29 16:49:35 -0400

    Add email uniqueness validator; add User test email must be unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
+  has_secure_password
+  
   validates :name, presence: true
   validates :email, presence: true
+  validates :email.downcase, uniqueness: true
   validates :password, presence: true
   validates :password_confirmation, presence: true
 
-  has_secure_password
+  validates_confirmation_of :password
 
+  # validates_uniqueness_of :email.downcase
+
+ 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,5 +21,33 @@ RSpec.describe User, type: :model do
       expect(@user).to_not be_valid
       expect(@user.errors.messages.include?(:password))
     end
+    it 'fails if different password in confirmation' do
+      @user = User.new({
+        name: 'Steve-o',
+        email: 'steve@steveplace.com',
+        password: 'STEEVE',
+        password_confirmation: 'STEEEVE'
+      })
+      expect(@user).to_not be_valid
+      expect(@user.errors.messages.include?(:password))
+    end
+  end
+  describe 'E-mail address' do
+    it 'must be unique' do
+      @user = User.create({
+        name: 'Steve-o',
+        email: 'steve@steveplace.com',
+        password: 'STEEVE',
+        password_confirmation: 'STEEVE'
+      })
+      @user2 = User.create({
+        name: 'steve',
+        email: 'steve@steveplace.com',
+        password: 'STVE',
+        password_confirmation: 'STVE'
+      })
+      expect(@user2).to_not be_valid
+      expect(@user2.errors.messages[:email]).to eq ["has already been taken"]
+    end
   end
 end
