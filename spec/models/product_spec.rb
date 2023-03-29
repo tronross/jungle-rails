13db8046dfebacf64c7f94386c8f43d5f2ec3f29
tronross <john_ross@analogforever.ca>
2023-03-29 13:21:29 -0400

    Refactor name Product price validation test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -13,9 +13,7 @@ RSpec.describe Product, type: :model do
       expect(@product).to be_valid
     end
 
-
-
-    it 'saves the correct name' do
+    it 'fails with invalid name' do
       @category = Category.new
       @product = Product.new({
         name: nil,
@@ -27,15 +25,15 @@ RSpec.describe Product, type: :model do
       expect(@product.errors.messages[:name]).to eq ["can't be blank"]
     end
 
-    it 'saves the correct price' do
+    it 'fails with invalid price' do
       @category = Category.new
       @product = Product.new({
         name: 'Boston',
-        price: 26.99,
         quantity: 5,
         category: @category
       })
-      expect(@product.price).to eq(Money.new(2699, :CAD))
+      expect(@product).to_not be_valid
+      expect(@product.errors.messages.include?(:price))
     end
 
     it 'saves the correct quantity' do
