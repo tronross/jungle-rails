1e2c92daa3eb0f75512fe6b168dbbf4fa78a57a3
tronross <john_ross@analogforever.ca>
2023-03-29 15:17:52 -0400

    Add User validation saves if all fields are valid

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it 'saves if all fields are valid' do
+  @user = User.new({
+    name: 'Steve-o',
+    email: 'steve@steveplace.com',
+    password: 'STEEVE',
+    password_confirmation: 'STEEVE'
+  })
+  expect(@user).to be_valid
+  end
   describe 'Password' do
     it 'fails if missing password' do
       @user = User.new({
@@ -10,7 +19,7 @@ RSpec.describe User, type: :model do
         password_confirmation: 'STEEVE'
       })
       expect(@user).to_not be_valid
-      expect(@user.errors.messages[:name]).to eq ["can't be blank"]
+      expect(@user.errors.messages.include?(:password))
     end
   end
 end
