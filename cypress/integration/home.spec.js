49aa5a5a45278680d44449e987d3d0555ec2b124
tronross <john_ross@analogforever.ca>
2023-03-31 04:05:56 -0400

    Add product count to test

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -12,4 +12,8 @@ describe('Jungle', () => {
   it("There are products on the page", () => {
     cy.get(".products article").should("be.visible");
   });
+
+  it("There are 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
 })
