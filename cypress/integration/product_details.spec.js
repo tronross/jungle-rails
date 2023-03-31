668feebd4c2a7be3ddbed473c2a4f7a01337bbab
tronross <john_ross@analogforever.ca>
2023-03-31 06:43:02 -0400

    Lint product_details.spec

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -1,13 +1,13 @@
-describe('Jungle product view', () => {
+describe('Product details', () => {
   beforeEach(() => {
     cy.visit('/')
   })
 
-  it('visits the home page on load', () => {
+  it('Visits the home page on load', () => {
     cy.get('.navbar-brand').should('be.visible');
   });
 
-  it('loads an individual product page when product is selected', () => {
+  it('Loads the product details page when image is clicked on home page', () => {
     cy.get('[alt="Scented Blade"]').click()
     cy.get('p').should('have.text', '\n    The Scented Blade is an extremely rare, tall plant and can be found mostly in savannas. It blooms once a year, for 2 weeks.© 2021 Jungle, Inc')
   });
