f162212f354ba78bbfd0f10e984ec5dfdda07e53
tronross <john_ross@analogforever.ca>
2023-03-31 05:55:06 -0400

    Lint home.spec

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,19 +1,17 @@
-
-
-describe('Jungle', () => {
+describe('Jungle home page', () => {
   beforeEach(() => {
     cy.visit('/')
   })
 
-  it('visits the home page on load', () => {
-    cy.get(".navbar-brand").should("be.visible");
+  it('Visits the home page on load', () => {
+    cy.get('.navbar-brand').should('be.visible');
   });
 
-  it("There are products on the page", () => {
-    cy.get(".products article").should("be.visible");
+  it('There are products on the page', () => {
+    cy.get('.products article').should('be.visible');
   });
 
-  it("There are 2 products on the page", () => {
-    cy.get(".products article").should("have.length", 2);
+  it('There are 2 products on the page', () => {
+    cy.get('.products article').should('have.length', 2);
   });
 })
